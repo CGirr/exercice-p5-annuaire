ca043a6bfa12ebc7a870fb1266d029fd14e75d0d
Coraline Girr <coraline.girr@gmail.com>
2025-06-26 17:44:59 +0200

    replaced \n with PHP_EOL and cleaned up echo with sprintf

--- Commands.php
+++ Commands.php
@@ -8,79 +8,77 @@ class Commands
 {
     /**
      * Fonction pour afficher la liste de tous les contacts
-     * @return void
      */
     public static function list(): void
     {
         $contactManager = new ContactManager();
         $contacts = $contactManager->findAll();
-        echo "affichage de la liste des contacts \n";
+        echo "affichage de la liste des contacts" . PHP_EOL;
         foreach ($contacts as $contact) {
-            echo $contact . "\n";
+            echo $contact . PHP_EOL;
         }
     }
 
     /**
      * Fonction pour afficher les détails d'un contact à partir de son ID
-     * @param int $id
-     * @return void
      */
     public static function detail(int $id): void
     {
         $contactManager = new ContactManager();
         $contact = $contactManager->findById($id);
         if (isset($contact)) {
-            echo "affichage du contact \n";
-            echo $contact . "\n";
+            echo "affichage du contact" . PHP_EOL;
+            echo $contact . PHP_EOL;
         } else {
-            echo "Cet id n'existe pas !\n";
+            echo "Cet id n'existe pas !" . PHP_EOL;
         }
     }
 
     /**
      * Fonction pour créer un contact
-     * @param string $name
-     * @param string $email
-     * @param string $phone
-     * @return void
      */
     public static function create(string $name, string $email, string $phone): void
     {
         $contactManager = new ContactManager();
         $contactManager->create($name, $email, $phone);
-        echo "Contact ajouté avec succès ! " . $name . " " . $email . " " . $phone . "\n";
+        echo sprintf("Contact ajouté avec succès ! %s %s %s", $name, $email, $phone) . PHP_EOL;
     }
 
     /**
      * Fonction pour supprimer un contact à partir de son ID
-     * @param int $id
-     * @return void
      */
     public static function delete(int $id): void
     {
         $contactManager = new ContactManager();
         $contactManager->delete($id);
-        echo "Le contact n°".$id." a été supprimé !\n";
+        echo sprintf("Le contact n°%d a été supprimé !", $id) . PHP_EOL;
     }
 
     /**
      * Fonction pour modifier un contact
-     * @param int $id
-     * @return void
      */
     public static function update(int $id, string $name, string $email, string $phone): void
     {
         $contactManager = new ContactManager();
         $contactManager->update($id, $name, $email, $phone);
-        echo "Le contact a été mis à jour avec succès !\n";
+        echo "Le contact a été mis à jour avec succès !" . PHP_EOL;
     }
 
     /**
      * Fonction affichant la liste des commandes
-     * @return void
      */
     public static function help(): void
     {
-        echo "Voici la liste des commandes :\nhelp : affiche cette aide\nexit : quitte l'application\nlist : affiche la liste de tous les contacts\ndetail [id] : affiche le détail du contact\ncreate [nom], [email], [phone number] : crée un contact\ndelete [id] : supprime le contact\nupdate [id] : modifie le contact\n";
+        echo <<<TEXT
+        Voici la liste des commandes :
+           help : affiche cette aide
+           exit : quitte l'application
+           list : affiche la liste des contacts
+           detail [id] : affiche le détail du contact
+           create [nom], [email], [phone number] : crée un contact
+           delete [id] : supprime le contact
+           update [id], [nom], [email], [phone number] : modifie le contact
+        
+        TEXT;
     }
 }
